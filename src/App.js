fd2eb851df7bfb61b29656aefe7fe75ae51486fd
Soul Lee <lee.soul12@gmail.com>
2022-08-25 18:19:01 -0400

    add a feature that lets you see the detail after clicking on the card on the map

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ const App = () => {
   const [places, setPlaces] = useState([]);
   const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState({});
+  const [childClicked, setChildClicked] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    console.log(bounds);
     navigator.geolocation.getCurrentPosition(
       ({ coords: { latitude, longitude } }) => {
         setCoordinates({ lat: latitude, lng: longitude });
@@ -22,9 +23,10 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log(coordinates, bounds);
+    setIsLoading(true);
     getPlacesData(bounds.sw, bounds.ne).then((data) => {
       setPlaces(data);
+      setIsLoading(false);
     });
   }, [coordinates, bounds]);
   return (
@@ -33,13 +35,19 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List places={places} />
+          <List
+            places={places}
+            childClicked={childClicked}
+            isLoading={isLoading}
+          />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
+            setChildClicked={setChildClicked}
           />
         </Grid>
       </Grid>
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -6,8 +6,10 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const PlaceDetails = ({ place }) => {
+const PlaceDetails = ({ place, selected, refProp }) => {
   const classes = useStyles();
+
+  if(selected) refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start"})
 
   return (
     <Card elevation={6}>
@@ -23,6 +25,10 @@ const PlaceDetails = ({ place }) => {
           <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
         </Box>
         <Box display="flex" justifyContent="space-between">
+        <Rating value={Number(place.rating)} readOnly />
+          <Typography gutterBottom variant="subtitle1">out of {place.num_reviews} reviews</Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
           <Typography variant="subtitle1">Ranking</Typography>
           <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
         </Box>
@@ -35,6 +41,25 @@ const PlaceDetails = ({ place }) => {
         {place?.cuisine?.map(({ name }) => (
           <Chip key={name} size="small" label={name} className={classes.chip}/>
         ))}
+        {place?.address && (
+          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.subtitle2}>
+            <LocationOnIcon /> {place.address}
+          </Typography>
+        )}
+        {place?.phone && (
+          <Typography gutterBottom variant="body2" color="textSecondary" className={classes.spacing}>
+            <PhoneIcon /> {place.phone}
+          </Typography>
+        )}
+        <CardActions>
+          <Button size="small" color="primary" onClick={() => window.open(place.web_url, '_blank')}>
+            Trip Advisor
+          </Button>
+          <Button size="small" color="primary" onClick={() => window.open(place.website, '_blank')}>
+            Website
+          </Button>
+        </CardActions>
+
       </CardContent>
     </Card>
   );
